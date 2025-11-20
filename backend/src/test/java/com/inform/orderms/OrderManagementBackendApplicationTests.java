2a54b526849c12a87a7680b2f08699298451fda1
Carlos Rojas <crojas@tracktec.cl>
2025-11-20 15:53:30 -0300

    add hello world java test

--- backend/src/test/java/com/inform/orderms/OrderManagementBackendApplicationTests.java
+++ backend/src/test/java/com/inform/orderms/OrderManagementBackendApplicationTests.java
@@ -10,4 +10,11 @@ class OrderManagementBackendApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void helloWorldTest() {
+		String greeting = "Hello World from Java 21!";
+		assert greeting.contains("Java 21");
+		System.out.println(greeting);
+	}
+
 }
